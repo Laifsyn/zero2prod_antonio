4b7a3e9ed66432bc8d9b886cd263d32bb3cf0e15
Laifsyn <laifzing@gmail.com>
2023-11-08 18:54:05 -0500

    <3.4-Our First Integration Test
    Write a simple "health_check" function which will be used to check the
    server's state through a specific endpoint in the server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("arbitrary_name").unwrap_or("World");
     format!("Hello {}!", &name)
@@ -9,10 +9,15 @@ async fn main() -> Result<(), std::io::Error> {
 
     HttpServer::new(|| {
         App::new()
+            .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{arbitrary_name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
     .await
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
