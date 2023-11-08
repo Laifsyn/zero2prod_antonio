d81641be22ab2796a46b61a0a05e6f7d2704c153
Laifsyn <laifzing@gmail.com>
2023-11-08 17:58:33 -0500

    Update the "{name}" placeholder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
+    let name = req.match_info().get("arbitrary_name").unwrap_or("World");
     format!("Hello {}!", &name)
 }
 #[tokio::main]
@@ -10,7 +10,7 @@ async fn main() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/{arbitrary_name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
