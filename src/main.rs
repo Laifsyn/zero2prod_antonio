bff05ae1e0e4fd57f60df32b123ca2546535365b
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2023-11-29 15:13:33 -0500

    =4.3.3-The Facade Pattern

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use env_logger::Env;
 use zero2prod_antonio::startup::run;
 use zero2prod_antonio::{bind_port, configuration, get_connection_to_database, LOCAL_HOST_IP};
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let connection = get_connection_to_database();
     let server_port = configuration::get_configuration().unwrap().application_port;
     let listener = bind_port(format!("{LOCAL_HOST_IP}:{}", server_port));
--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,5 @@
 use crate::routes;
+use actix_web::middleware::Logger;
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -7,6 +8,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .route("/{arbitrary_name}", web::get().to(routes::greet))
