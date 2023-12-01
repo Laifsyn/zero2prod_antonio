c6e4e55ccaf9073175a304de2393f347cdcacd5c
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2023-12-01 10:28:46 -0500

    =4.4.3 A way to Differentiate each Async Log
    For those who has windows, you can use this curl command I'd used to display the request ID
    
    ```
    curl http://127.0.0.1:7878/subscriptions ^
    -X POST ^
    -d "email=Esquema_mann@gmail.com&name=Tom"
    ```

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,8 +10,13 @@ pub struct UserEmail {
 }
 
 pub async fn subscribe(form: web::Form<UserEmail>, connection: web::Data<PgPool>) -> HttpResponse {
-    log::info!("captured email:\"{}\", name: \"{}\"", form.email, form.name);
-    log::info!("Saving new subscriber details in the database");
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "Request ID {request_id} - captured email:\"{}\", name: \"{}\"",
+        form.email,
+        form.name
+    );
+    log::info!("Request ID {request_id} - Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -26,12 +31,12 @@ pub async fn subscribe(form: web::Form<UserEmail>, connection: web::Data<PgPool>
     .await
     {
         Ok(_) => {
-            log::info!("New subscriber details have been saved");
+            log::info!("Request ID {request_id} - New subscriber details have been saved");
             HttpResponse::Ok().finish()
         }
         Err(error) => {
             // #[cfg(debug_assertions)]
-            log::error!("Error from SQL: {error:?}\nEnd of Error...",);
+            log::error!("Request ID {request_id} - Error from SQL: {error:?}\nEnd of Error...",);
             HttpResponse::InternalServerError().finish()
         }
     }
