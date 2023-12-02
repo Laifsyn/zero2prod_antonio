7e3a2d4b9d16656648ad446535312009303a922c
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2023-12-02 15:24:31 -0500

    =4.5.12-Cleaning Up Instrumentation Code - tracing::instrument
    Refactor Subscribe function. We attach a request span onto the subscribe function, so we clear the tracing boilerplate from the function body

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,22 +10,17 @@ pub struct UserEmail {
     email: String,
 }
 
-pub async fn subscribe(form: web::Form<UserEmail>, connection: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
         subscriber_email = %form.email,
         subscriber_name = %form.name
-    );
-    let _request_span_guard = request_span.enter();
+    )
+)]
+pub async fn subscribe(form: web::Form<UserEmail>, pool: web::Data<PgPool>) -> HttpResponse {
     let query_span = tracing::info_span!("Saving new subscriber details in the database");
-    tracing::info!(
-        "Request ID {request_id} - captured email:\"{}\", name: \"{}\"",
-        form.email,
-        form.name
-    );
-    tracing::info!("Request ID {request_id} - Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -36,18 +31,11 @@ pub async fn subscribe(form: web::Form<UserEmail>, connection: web::Data<PgPool>
         form.name,
         Utc::now()
     )
-    .execute(connection.get_ref())
+    .execute(pool.get_ref())
     .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info!("Request ID {request_id} - New subscriber details have been saved");
-            HttpResponse::Ok().finish()
-        }
-        Err(error) => {
-            // #[cfg(debug_assertions)]
-            tracing::error!("Request ID {request_id} - Error from SQL: {error:?}\nEnd of Error...",);
-            HttpResponse::InternalServerError().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
