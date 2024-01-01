6dd0bb0d8221663eeda15576517d7121d57b947e
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2024-01-01 11:49:08 -0500

    staging changes

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,8 @@
 //! src/configuration.rs
 
+use serde_aux::field_attributes::deserialize_number_from_string;
+// [...]
+
 use secrecy::{ExposeSecret, Secret};
 #[derive(serde::Deserialize, Debug)]
 pub struct Settings {
@@ -11,12 +14,14 @@ pub struct Settings {
 pub struct DatabaseSettings {
     pub username: String,
     pub password: Secret<String>,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
 }
 #[derive(serde::Deserialize, Debug)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
 }
@@ -24,8 +29,7 @@ pub struct ApplicationSettings {
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
-    // Detect the running environment.
-    // Default to `local` if unspecified.
+
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
         .unwrap_or_else(|_| "local".into())
         .try_into()
@@ -38,11 +42,15 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(config::File::from(
             configuration_directory.join(environment_filename),
         ))
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        ) // `APP_APPLICATION__PORT=5001 would set `Settings.application.port` to 5001
         .build()?;
-    // Try to convert the configuration values it read into
-    // our Settings type
+
     let ret = settings.try_deserialize::<Settings>();
-    // #[cfg(debug_assertions)]
+    #[cfg(debug_assertions)]
     if let Ok(ret) = &ret {
         tracing::info!(
             "DB Connection Settings - application: {:?},  {:?}",
