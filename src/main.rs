91c9fbbfaded0470cf42f8918a5f49eae18ac311
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2024-01-07 13:47:17 -0500

    Squashed commits: Chapter 5
    commit b139f16bac9b9da9cc6124c81bc9163ed63a4a8b
    Author: Laifsyn <99366187+Laifsyn@users.noreply.github.com>
    Date:   Sun Jan 7 13:32:13 2024 -0500
    
        *5.4.5-Create a PG Connection via PgConnectOptions::new()
    
        It's important to remember I've skipped the steps of actually testing this in an actual third party app...
    
    commit 6dd0bb0d8221663eeda15576517d7121d57b947e
    Author: Laifsyn <99366187+Laifsyn@users.noreply.github.com>
    Date:   Mon Jan 1 11:49:08 2024 -0500
    
        staging changes
    
    commit 7fcc9077e2816c4ed543546ca3630424ca24c1cd
    Author: Laifsyn <99366187+Laifsyn@users.noreply.github.com>
    Date:   Sun Dec 24 21:50:22 2023 -0500
    
        No "changes". I continued on.....
    
        I couldn't afford to keep delaying my progress in the book, so I choose to skip the Docker compatibility issues for the futures.
    
    commit ddecc8a5215d983b8050c5bd0497538dec1acc08
    Author: Laifsyn <99366187+Laifsyn@users.noreply.github.com>
    Date:   Tue Dec 19 09:26:16 2023 -0500
    
        *5.3.8 Image Optimization - Scraping /target
    
        By docker-ignoring all the irrelevant files, we can end up with a ~100MB image for the Docker.
    
        Worth noting is that the Entrypoints are the binary name.
    
        Also, I couldn't figure how to manage how to tap into the health checkpoint from the host machine. I'll later upload an update on what changes were made to the project to be able to tap into the checkpoint when I can grab some time to read into someone's guide for the Docker Compose method.
    
    commit 7cdc6fc8b13bac2ca664bd3574f351bbafdc2b16
    Author: Laifsyn <99366187+Laifsyn@users.noreply.github.com>
    Date:   Fri Dec 15 09:31:45 2023 -0500
    
        *5.3.6-Hierarchical Configuration Staging Step
    
        Couldn't run the tests for post and healthcheck. I'll see what I can do after staging the steps for smaller docker image
    
    commit 654c060e1a8219051cd3d32845dcbc590143b61b
    Author: Laifsyn <99366187+Laifsyn@users.noreply.github.com>
    Date:   Thu Dec 14 09:15:15 2023 -0500
    
        *5.3.4-Running an Image
    
        -Change connection to lazy so it doesn't crash on start.
        -Enable the Cached SQLX so it doesn't fail when compiling the docker image

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use zero2prod_antonio::startup::run;
 use zero2prod_antonio::telemetry::{get_subscriber, init_subscriber};
-use zero2prod_antonio::{bind_port, get_connection_to_database, LOCAL_HOST_IP};
+use zero2prod_antonio::{bind_port, get_connection_to_database};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let subscriber = get_subscriber("zero2prod_antonio".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
-
-    let (connection, server_port) = get_connection_to_database().await;
-    let listener = bind_port(format!("{LOCAL_HOST_IP}:{}", server_port));
+    let (connection, server_connection) = get_connection_to_database().await;
+    let listener = bind_port(format!(
+        "{}:{}",
+        server_connection.application.host, server_connection.application.port
+    ));
     tracing::info!("Server started: {}", listener.local_addr().unwrap()); // Runtime Logging
     let server = run(listener, connection)?;
     tokio::spawn(async move {
