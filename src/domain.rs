a56d49d719d833fcad13287406478be87f00b9bb
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2024-01-09 14:31:24 -0500

    6.9 Unit Tests
    I chose to call a static array of invalid characters to avoid desync of invalid characters between build and test

--- src/domain.rs
+++ src/domain.rs
@@ -4,15 +4,17 @@ pub struct NewSubscriber {
     pub email: String,
     pub name: SubscriberName,
 }
+#[derive(Debug)]
 pub struct SubscriberName(String);
 impl SubscriberName {
+    const INVALID_CHARACTERS: &'static [char] = &['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
     unsafe fn parse(s: &str) -> SubscriberName {
         Self(s.to_string())
     }
     fn is_valid(s: &str) -> bool {
         let is_empty_or_whitespace = s.trim().is_empty();
         let is_too_long = s.graphemes(true).count() > 256;
-        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let forbidden_characters = Self::INVALID_CHARACTERS;
         let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
 
         !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
@@ -29,8 +31,63 @@ impl TryFrom<&str> for SubscriberName {
         }
     }
 }
+
+impl TryFrom<String> for SubscriberName {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match SubscriberName::is_valid(value.as_str()) {
+            true => Ok(unsafe { SubscriberName::parse(value.as_str()) }),
+            false => Err(format!("{:?} is not a valid subscriber name", value)),
+        }
+    }
+}
 impl AsRef<str> for SubscriberName {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberName;
+    use claims::{assert_err, assert_ok};
+
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "ё".repeat(256);
+        assert_ok!(SubscriberName::try_from(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::try_from(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::try_from(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::try_from(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in SubscriberName::INVALID_CHARACTERS {
+            let name = name.to_string();
+            assert_err!(SubscriberName::try_from(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "Ursula Le Guin".to_string();
+        assert_ok!(SubscriberName::try_from(name));
     }
 }
